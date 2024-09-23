6422af46ffae2f27f6e089822f3c9a1c82e68338
dapopov-st <dpopovvelasco@gmail.com>
2024-09-23 13:50:48 -0400

    filter synthetic data, initial sfttrainer setup

---
+++ utils.py
@@ -0,0 +1,33 @@
+import sys,gc,traceback
+import torch
+def clean_ipython_hist():
+    # Code in this function mainly copied from IPython source
+    if not 'get_ipython' in globals(): return
+    ip = get_ipython()
+    user_ns = ip.user_ns
+    ip.displayhook.flush()
+    pc = ip.displayhook.prompt_count + 1
+    for n in range(1, pc): user_ns.pop('_i'+repr(n),None)
+    user_ns.update(dict(_i='',_ii='',_iii=''))
+    hm = ip.history_manager
+    hm.input_hist_parsed[:] = [''] * pc
+    hm.input_hist_raw[:] = [''] * pc
+    hm._i = hm._ii = hm._iii = hm._i00 =  ''
+
+
+
+def clean_tb():
+    # h/t Piotr Czapla
+    if hasattr(sys, 'last_traceback'):
+        traceback.clear_frames(sys.last_traceback)
+        delattr(sys, 'last_traceback')
+    if hasattr(sys, 'last_type'): delattr(sys, 'last_type')
+    if hasattr(sys, 'last_value'): delattr(sys, 'last_value')
+
+def clean_mem():
+    clean_tb()
+    clean_ipython_hist()
+    gc.collect()
+    for device in range(torch.cuda.device_count()):
+        torch.cuda.set_device(device)
+        torch.cuda.empty_cache()
